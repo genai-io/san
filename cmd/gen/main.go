2b99534613a1aaac23fca170516dcf8721f4e1e3
Meng Yan <myan@redhat.com>
2026-05-02 18:32:09 +0800

    chore: bump version to 0.0.2
    Signed-off-by: Meng Yan <myan@redhat.com>

--- cmd/gen/main.go
+++ cmd/gen/main.go
@@ -24,7 +24,7 @@ import (
 	_ "github.com/genai-io/gen-code/internal/llm/openai"
 )
 
-var version = "0.0.1"
+var version = "0.0.2"
 
 // cliOpts holds all CLI flag values in one place.
 var cliOpts struct {
