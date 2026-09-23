e95e826357caa220e046541a844f9291d40aedd3
Meng Yan <yanmxa@gmail.com>
2026-09-23 21:18:46 +0800

    chore: bump version to 1.22.11
    Signed-off-by: Meng Yan <yanmxa@gmail.com>

--- cmd/san/main.go
+++ cmd/san/main.go
@@ -17,7 +17,7 @@ import (
 	"github.com/genai-io/san/internal/setting"
 )
 
-var version = "1.22.10"
+var version = "1.22.11"
 
 // buildTime and commit are set at build time via -X ldflags.
 // When built directly with go build (without ldflags), they remain empty.
